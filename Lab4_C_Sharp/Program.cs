3cf6e5c292507dcea567e5974ebf12ada89952f7
Havanji <andriyparayko2017@gmail.com>
2022-11-02 09:56:09 +0200

    Add project files.

---
+++ Lab4_C_Sharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab4_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(25);
+
+            Console.WriteLine("Площа кола:" + circle1.Area());
+            Console.WriteLine("Площа кола:" + circle2.Area());
+            Console.WriteLine("Периметр кола:" + circle1.Perimeter());
+            Console.WriteLine("Периметр кола:" + circle2.Perimeter());
+
+            Square square1 = new Square(5);
+            Square square2 = new Square(25);
+
+            Console.WriteLine("Площа квадрату:" + square1.Area());
+            Console.WriteLine("Площа квадрату:" + square2.Area());
+            Console.WriteLine("Периметр квадрвту:" + square1.Perimeter());
+            Console.WriteLine("Периметр квадрвту:" + square2.Perimeter());
+
+            
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab4_C_Sharp/Shape.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+abstract public class Shape
+{
+    private string name;
+
+    public string Name
+	{
+		get { return name; }
+		set { name = value; }
+	}
+
+	public void Figure(string Name)
+	{
+		Console.WriteLine($"Це {this.Name}");
+	}
+
+	public abstract double Area();
+
+	public abstract double Perimeter();
+}
+
+public class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public override double Area() 
+	{
+        double area;
+        area = Math.PI * this.radius;
+        return area;
+	}
+
+    public override double Perimeter()
+    {
+        double perimeter;
+        perimeter = 2* Math.PI * this.radius;
+        return perimeter;
+    }
+}
+
+public class Square : Shape
+{
+    private double side;
+
+    public Square(double side)
+    {
+        this.side = side;
+    }
+
+    public override double Area()
+    {
+        double area;
+        area = 4 * this.side;
+        return area;
+    }
+
+    public override double Perimeter()
+    {
+        double perimeter;
+        perimeter = this.side * this.side;
+        return perimeter;
+    }
+
+}
