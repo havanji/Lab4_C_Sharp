d7c4d18a56e710e6b50db1e9407c28deaff4cbcc
Havanji <andriyparayko2017@gmail.com>
2022-11-06 21:28:24 +0200

    all tasks

--- Lab4_C_Sharp/Program.cs
+++ Lab4_C_Sharp/Program.cs
@@ -6,6 +6,10 @@ namespace Lab4_C_Sharp
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+
             Circle circle1 = new Circle(5);
             Circle circle2 = new Circle(25);
 
@@ -19,13 +23,27 @@ namespace Lab4_C_Sharp
 
             Console.WriteLine("Площа квадрату:" + square1.Area());
             Console.WriteLine("Площа квадрату:" + square2.Area());
-            Console.WriteLine("Периметр квадрвту:" + square1.Perimeter());
-            Console.WriteLine("Периметр квадрвту:" + square2.Perimeter());
+            Console.WriteLine("Периметр квадрату:" + square1.Perimeter());
+            Console.WriteLine("Периметр квадрату:" + square2.Perimeter());
+
+            if (square1.Perimeter() > square2.Perimeter())
+            {
+                Console.WriteLine("Більшим периметром квадрату є " + square1.Area());
+            }
+            else
+            {
+                Console.WriteLine("Більшим  периметром квадрату є " + square2.Area());
+            }
 
             
-
-
-
+            if (circle1.Perimeter() > circle2.Perimeter())
+            {
+                Console.WriteLine("Більшим периметром кола є " + circle1.Area());
+            }
+            else
+            {
+                Console.WriteLine("Більшим периметром кола є " + circle2.Area());
+            }
 
             Console.ReadKey();
         }
--- Lab4_C_Sharp/Shape.cs
+++ Lab4_C_Sharp/Shape.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
 using System.Security.Cryptography;
 
+//Основний батьківсткий клас
 abstract public class Shape
 {
     private string name;
@@ -22,8 +24,10 @@ abstract public class Shape
 	public abstract double Perimeter();
 }
 
+//Коло (Sircle), нащадок Shape
 public class Circle : Shape
-{
+{   
+    //Радіус для подаальшого обчислення
     private double radius;
 
     public Circle(double radius)
@@ -31,23 +35,28 @@ public class Circle : Shape
         this.radius = radius;
     }
 
+    //Переписаний Area для обслення площі 
     public override double Area() 
 	{
         double area;
-        area = Math.PI * this.radius;
+        area = Math.Round(Math.PI * this.radius, 2);
         return area;
 	}
+
+    //Переписаний Perimeter для обслення ериметру 
 
     public override double Perimeter()
     {
         double perimeter;
-        perimeter = 2* Math.PI * this.radius;
+        perimeter = Math.Round(2* Math.PI * this.radius, 2);
         return perimeter;
     }
 }
 
+//Квадрат (Square), нащадок Shape
 public class Square : Shape
 {
+    //Сторона для подальшого обчислення
     private double side;
 
     public Square(double side)
@@ -55,18 +64,19 @@ public class Square : Shape
         this.side = side;
     }
 
+    //Переписаний Area для обслення площі 
     public override double Area()
     {
         double area;
-        area = 4 * this.side;
+        area = Math.Round(4 * this.side, 2);
         return area;
     }
 
+    //Переписаний Perimeter для обслення ериметру 
     public override double Perimeter()
     {
         double perimeter;
-        perimeter = this.side * this.side;
+        perimeter = Math.Round(this.side * this.side, 2);
         return perimeter;
     }
-
 }
